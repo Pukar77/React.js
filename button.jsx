02fd4fa8a33a67386008bbba81b22390bd84747b
Pukar Rimal <110043874+Pukar77@users.noreply.github.com>
2023-12-13 22:20:43 +0545

    Add files via upload

---
+++ button.jsx
@@ -0,0 +1,8 @@
+function btn(){
+    return(<div> 
+<h5>Hello, click on this button to see me</h5>
+<button>Click here</button>
+    </div>)
+}
+
+export default btn;
---
+++ hello.jsx
@@ -0,0 +1,7 @@
+function Haha(){
+    return <div>
+    <p>Hello, i am pukar, i am just 2 month before becoming froentend developer</p>
+    </div>
+}
+
+export default Haha;
